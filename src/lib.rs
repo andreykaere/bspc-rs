94f4ec42bf799207980a5ac9828e716e877adcdf
Andrey <andreykaere@gmail.com>
2023-02-24 22:01:14 +0300

    Add test for subscribe, init some basic struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,73 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::error::Error;
+
+pub enum MonitorEvent {
+    MonitorAdd,
+    MonitorRename,
+    MonitorSwap,
+    MonitorFocus,
+    MonitorGeometry,
+}
+
+pub enum DesktopEvent {
+    DesktopAdd,
+    DesktopRename,
+    DesktopRemove,
+    DesktopSwap,
+    DesktopTransfer,
+    DesktopFocus,
+    DesktopActivate,
+    DesktopLayout,
+}
+
+pub enum NodeEvent {
+    NodeAdd,
+    NodeRemove,
+    NodeSwap,
+    NodeTransfer,
+    NodeFocus,
+    NodeActivate,
+    NodePresel,
+    NodeStack,
+    NodeGeometry,
+    NodeState,
+    NodeFlag,
+    NodeLayer,
+}
+
+pub enum Event {
+    Report,
+    Monitor(MonitorEvent),
+    Dekstop(DesktopEvent),
+    Node(NodeEvent),
+    PointerAction,
+}
+
+pub fn subscribe(foo: i32, events: &[Event]) -> Result<(), Box<dyn Error>> {
+    todo!();
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod test {
+    use std::error::Error;
+    use std::io::{self, Read, Write};
+    use std::os::unix::net::UnixStream;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_subscribe() -> Result<(), Box<dyn Error>> {
+        let mut stream = UnixStream::connect("/tmp/bspwm_0_0-socket")?;
+        let mut buf = [0u8; 1024];
+
+        stream.write_all(b"subscribe\x00node_remove\x00")?;
+
+        loop {
+            let len = stream.read(&mut buf)?;
+            if len == 0 {
+                break;
+            }
+            let response = String::from_utf8_lossy(&buf[..len]);
+            println!("{}", response);
+        }
+
+        Ok(())
     }
 }
