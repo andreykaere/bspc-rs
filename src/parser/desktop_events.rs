b3c152a7af63764d9410e86de2bc1f8d681d1b3f
Andrey <andreykaere@gmail.com>
2023-03-11 15:30:37 +0300

    Implement parser for desktop and monitor events

--- src/parser/desktop_events.rs
+++ src/parser/desktop_events.rs
@@ -1,1 +1,116 @@
+use super::super::events::*;
+use super::errors::ParseError;
+use super::utils::{from_hex, get_event_type, process_event_reply};
 
+use std::str::FromStr;
+
+impl FromStr for DesktopAddInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "desktop_add", 3)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+            desktop_id: from_hex(reply[2])?,
+            desktop_name: reply[3].to_string(),
+        })
+    }
+}
+
+impl FromStr for DesktopRenameInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "desktop_rename", 4)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+            desktop_id: from_hex(reply[2])?,
+            old_name: reply[3].to_string(),
+            new_name: reply[4].to_string(),
+        })
+    }
+}
+
+impl FromStr for DesktopRemoveInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "desktop_remove", 2)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+            desktop_id: from_hex(reply[2])?,
+        })
+    }
+}
+
+impl FromStr for DesktopSwapInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "desktop_swap", 4)?;
+
+        Ok(Self {
+            src_monitor_id: from_hex(reply[1])?,
+            src_desktop_id: from_hex(reply[2])?,
+            dst_monitor_id: from_hex(reply[3])?,
+            dst_desktop_id: from_hex(reply[4])?,
+        })
+    }
+}
+
+impl FromStr for DesktopTransferInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "desktop_transfer", 3)?;
+
+        Ok(Self {
+            src_monitor_id: from_hex(reply[1])?,
+            src_desktop_id: from_hex(reply[2])?,
+            dst_monitor_id: from_hex(reply[3])?,
+        })
+    }
+}
+
+impl FromStr for DesktopFocusInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "desktop_focus", 2)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+            desktop_id: from_hex(reply[2])?,
+        })
+    }
+}
+
+impl FromStr for DesktopActivateInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "desktop_activate", 2)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+            desktop_id: from_hex(reply[2])?,
+        })
+    }
+}
+
+impl FromStr for DesktopLayoutInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "desktop_layout", 2)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+            desktop_id: from_hex(reply[2])?,
+            layout: reply[3].parse()?,
+        })
+    }
+}
--- src/parser/monitor_events.rs
+++ src/parser/monitor_events.rs
@@ -1,1 +1,83 @@
+use super::super::events::*;
+use super::errors::ParseError;
+use super::utils::{from_hex, get_event_type, process_event_reply};
 
+use std::str::FromStr;
+
+impl FromStr for MonitorAddInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "monitor_add", 3)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+            monitor_name: reply[2].to_string(),
+            monitor_geometry: reply[3].parse()?,
+        })
+    }
+}
+
+impl FromStr for MonitorRenameInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "monitor_rename", 3)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+            old_name: reply[2].to_string(),
+            new_name: reply[3].to_string(),
+        })
+    }
+}
+
+impl FromStr for MonitorRemoveInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "monitor_remove", 1)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+        })
+    }
+}
+
+impl FromStr for MonitorSwapInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "monitor_swap", 2)?;
+
+        Ok(Self {
+            src_monitor_id: from_hex(reply[1])?,
+            dst_monitor_id: from_hex(reply[2])?,
+        })
+    }
+}
+
+impl FromStr for MonitorFocusInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "monitor_focus", 1)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+        })
+    }
+}
+
+impl FromStr for MonitorGeometryInfo {
+    type Err = ParseError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let reply = process_event_reply(input, "monitor_geometry", 2)?;
+
+        Ok(Self {
+            monitor_id: from_hex(reply[1])?,
+            monitor_geometry: reply[2].parse()?,
+        })
+    }
+}
